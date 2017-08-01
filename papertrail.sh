07cdcc7542bc5cf998718996311b81f2dedf05df
oliphunt <richard.brear@crossrhythms.co.uk>
2017-08-01 16:04:52 +0100

    Do env variable substitution as non-root user.

--- papertrail.sh
+++ papertrail.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # prepare the remote_syslog2 config file
-cp ./log_files.yml /etc/log_files.yml
-sed -i 's/{{ HOST }}/'$PAPERTRAIL_HOST'/g' /etc/log_files.yml
-sed -i 's/{{ PORT }}/'$PAPERTRAIL_PORT'/g' /etc/log_files.yml
+cp ./log_files.yml ./temp_log_files.yml
+sed -i 's/{{ HOST }}/'$PAPERTRAIL_HOST'/g' ./temp_log_files.yml
+sed -i 's/{{ PORT }}/'$PAPERTRAIL_PORT'/g' ./temp_log_files.yml
+sudo cp ./temp_log_files.yml /etc/log_files.yml
 
 # Prepare the init script
-cp ./remote_syslog.init.d /etc/init.d/remote_syslog
-chmod +x /etc/init.d/remote_syslog
+sudo cp ./remote_syslog.init.d /etc/init.d/remote_syslog
+sudo chmod +x /etc/init.d/remote_syslog
 
 # Download and install the remote_syslog2 binary from papertrail
 wget https://github.com/papertrail/remote_syslog2/releases/download/v0.19/remote_syslog_linux_amd64.tar.gz
 tar xzf ./remote_syslog*.tar.gz
 cd remote_syslog
-cp ./remote_syslog /usr/local/bin
+sudo cp ./remote_syslog /usr/local/bin
 
 # Start the service
-service remote_syslog start
-update-rc.d remote_syslog defaults
+sudo service remote_syslog start
+sudo update-rc.d remote_syslog defaults
